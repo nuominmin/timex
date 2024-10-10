01dd59a3fab54abb34663afde7c4722a6c9ac54c
nuominmin <776191753@qq.com>
2024-10-10 16:56:00 +0800

    feat: Add timex

---
+++ README.md
@@ -0,0 +1,4 @@
+# timex 
+
+timex is a utility package for time-related operations, offering convenient functions to work with time zones, current time, and date ranges like today's range or the current week's range. It extends Go's standard time package to provide enhanced time handling, especially for applications in the Asia/Shanghai timezone.
+
---
+++ timex.go
@@ -0,0 +1,44 @@
+package timex
+
+import (
+	"time"
+)
+
+func Loc() *time.Location {
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	return loc
+}
+
+func Now() time.Time {
+	return time.Now().In(Loc())
+}
+
+func NowUnix() int64 {
+	return time.Now().In(Loc()).Unix()
+}
+
+// ThisWeekRange returns the start and end time of this week
+func ThisWeekRange() (start time.Time, end time.Time) {
+	loc := Loc()
+	now := time.Now().In(loc)
+	weekday := now.Weekday()
+
+	daysSinceSunday := int(weekday)
+
+	// get last sunday date
+	sundayDate := now.AddDate(0, 0, -daysSinceSunday)
+	year, month, day := sundayDate.Date()
+
+	start = time.Date(year, month, day, 0, 0, 0, 0, loc)
+
+	// end date is start + 7 days - 1 second
+	end = start.Add(7*24*time.Hour - time.Second)
+	return start, end
+}
+
+func TodayRange() (start time.Time, end time.Time) {
+	loc := Loc()
+	now := time.Now().In(loc)
+	start = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
+	return start, start.Add(24*time.Hour - time.Second)
+}
